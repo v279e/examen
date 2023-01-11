7f5a87fcec23fff7c1901b436f9f343a0fb052ca
v279e <71925316+v279e@users.noreply.github.com>
2023-01-11 12:04:15 +0300

    Create test_api.py

---
+++ web/app/test_api.py
@@ -0,0 +1,27 @@
+import pytest
+from app import client
+
+#тест на доступность главной страницы
+def test_index():
+    response = client.get('/')
+    assert response.status_code == 200
+#тест на доступность страницы авторизации    
+def test_login():
+    response = client.get('/auth/login')
+    assert response.status_code == 200
+#тест на возможность входа с правильными данными
+def test_valid_auth():
+    data={"login": "xxxnagibator",
+          "password" : "qwerty"}
+    response = client.post('/auth/login', data=data)
+    assert response.status_code == 302
+#тест на невозможность входа с неправильными данными
+def test_invalid_auth():
+    data={"login": "v279e",
+          "password" : "user256"}
+    response = client.post('/auth/login', data=data)
+    assert response.status_code == 200
+#тест на возможность выхода из аккаунта
+def test_logout():
+    response=client.get('/auth/logout')
+    assert response.status_code==302
